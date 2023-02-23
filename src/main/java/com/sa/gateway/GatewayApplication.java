493970789b17b5503946547f688ec457de91793a
Faizul Noor <faizul.noor@hotmail.com>
2023-02-22 20:20:47 -0500

    Add new configuration

--- src/main/java/com/sa/gateway/GatewayApplication.java
+++ src/main/java/com/sa/gateway/GatewayApplication.java
@@ -1,9 +1,15 @@
 package com.sa.gateway;
 
+import java.security.NoSuchAlgorithmException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.context.annotation.Bean;
+
+import com.netflix.discovery.DiscoveryClient;
+import com.netflix.discovery.shared.transport.jersey.EurekaJerseyClientImpl.EurekaJerseyClientBuilder;
 
 //@CrossOrigin(origins = "http://localhost:4200")
 @SpringBootApplication
@@ -13,5 +19,23 @@ public class GatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
-
+/*	
+	@Bean
+	public DiscoveryClient.DiscoveryClientOptionalArgs discoveryClientOptionalArgs() throws NoSuchAlgorithmException {
+		DiscoveryClient.DiscoveryClientOptionalArgs args = new DiscoveryClient.DiscoveryClientOptionalArgs();
+		//System.setProperty("javax.net.ssl.keyStore", "src/main/resources/gateway.p12");
+		System.setProperty("javax.net.ssl.keyStore", "/tmp/gateway.p12");
+		System.setProperty("javax.net.ssl.keyStorePassword", "password");
+		//System.setProperty("javax.net.ssl.trustStore", "src/main/resources/gateway.p12");
+		System.setProperty("javax.net.ssl.trustStore", "/tmp/gateway.p12");
+		System.setProperty("javax.net.ssl.trustStorePassword", "password");
+		EurekaJerseyClientBuilder builder = new EurekaJerseyClientBuilder();
+		builder.withClientName("gateway");
+		builder.withSystemSSLConfiguration();
+		builder.withMaxTotalConnections(10);
+		builder.withMaxConnectionsPerHost(10);
+		args.setEurekaJerseyClient(builder.build());
+		return args;
+	}
+*/
 }
